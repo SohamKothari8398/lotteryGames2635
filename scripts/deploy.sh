f1a6bb03b40483cbe9d2bcb1ce21a1c21374c2ea
Tushar Kant <tushar.kant@mitplindia.com>
2024-01-29 12:34:11 +0530

    Added yarn binary path to deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 GIT_REPO=git@github.com:XSAVLAB/UP365-Gaming.git
 GIT_BRANCH=dev
+NODEJS_PATH=/etc/nodejs/current/
 
 git pull origin $GIT_BRANCH
 
@@ -11,13 +12,12 @@ yarn build
 # Stop existing frontend process
 pm2 stop frontend
 # Start frontend process
-pm2 --name frontend -i max start "yarn start"
+pm2 --name frontend -i max start "$NODEJS_PATH/bin/yarn start"
 
 cd ../backend
 yarn install
-yarn build
 # Stop existing backend process
 pm2 stop backend
 # Start backend process
-pm2 --name backend -i max start "yarn start"
+pm2 --name backend -i max start "$NODEJS_PATH/bin/yarn start"
 
