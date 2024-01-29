a343b3cc6260884234e6f8e4e8e7a3fdd21e573f
Tushar Kant <tushar.kant@mitplindia.com>
2024-01-29 13:04:04 +0530

    Shortened deployment step and put the source code step in workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-GIT_REPO=git@github.com:XSAVLAB/UP365-Gaming.git
-GIT_BRANCH=dev
-NODEJS_PATH=/etc/nodejs/current
-
-git pull origin $GIT_BRANCH
-
 cd frontend
 yarn install
 yarn build
-# Stop existing frontend process
-pm2 stop frontend
-# Start frontend process
-pm2 --name frontend -i max start "$NODEJS_PATH/bin/yarn start"
+pm2 restart frontend
 
 cd ../backend
 yarn install
-# Stop existing backend process
-pm2 stop backend
-# Start backend process
-pm2 --name backend -i max start "$NODEJS_PATH/bin/yarn start"
+pm2 restart backend
 
