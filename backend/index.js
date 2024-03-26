fd7acd82a9cc208e291377aab716d1e8bdacdff2
SohamKothari8398 <kotharisoham928@gamil.com>
2024-03-26 13:18:45 +0530

    CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -7,20 +7,6 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
-app.use(cors());
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["https://www.up365gaming.com", "https://api.up365gaming.com"],
-    // Same origin allowed for Socket.IO // origin: "https://up365gaming.com" || "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "PATCH"],
-    // Allowed methods for Socket.IO
-    credentials: true,
-    // Allow cookies for Socket.IO
-  },
-});
-
 // Importing Routers
 const UserRouter = require("./routes/userRouter");
 const PromosLinksRouter = require("./routes/promosLinksRouter");
@@ -32,29 +18,59 @@ const DoubleLotteryGameRouter = require("./routes/doubleLotteryGameRouter");
 const TripleLotteryGameRouter = require("./routes/tripleDigitLotteryRouter");
 const ColorballGameRouter = require("./routes/colorBallRouter");
 
-// Middleware
-app.use(express.json());
+const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://www.up365gaming.com",
+      "https://up365gaming.com",
+      "https://api.up365gaming.com",
+    ],
+    // Same origin allowed for Socket.IO // origin: "https://up365gaming.com" || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    // Allowed methods for Socket.IO
+    allowedHeaders: "*",
+    credentials: true,
+    // Allow cookies for Socket.IO
+  },
+});
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: [
+//       "https://www.up365gaming.com",
+//       "https://up365gaming.com",
+//       "https://api.up365gaming.com",
+//     ],
+// Same origin allowed for Socket.IO // origin: "https://up365gaming.com" || "http://localhost:3000",
+// methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+// Allowed methods for Socket.IO
+// credentials: true,
+// Allow cookies for Socket.IO
+//   },
+// });
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 app.use(cookieParser());
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 app.use("/", UserRouter);
 app.use("/", PromosLinksRouter);
 app.use("/", TransactionsRouter);
 app.use("/admin", AdminRoutes);
 app.use("/user/complaints", ComplaintsRouter);
-
 app.use("/games/singleDigitLottery", SingleLotteryGameRouter);
 app.use("/games/doubleDigitLottery", DoubleLotteryGameRouter);
 app.use("/games/tripleDigitLottery", TripleLotteryGameRouter);
 app.use("/games/colorBallLottery", ColorballGameRouter);
-
-io.on("connection", (socket) => {
-  console.log("New client connected");
-
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
 
 mongoose
   .connect(process.env.MONGO || "mongodb://localhost:27017/up365gaming")
