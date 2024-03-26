a4fa46f3de1aa82d98a20e717df0cd55c87804e1
SohamKothari8398 <kotharisoham928@gamil.com>
2024-03-26 13:52:33 +0530

    CORS Policy Change URLs Added

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "https://www.up365gaming.com",
-      "https://up365gaming.com",
-      "http://www.up365gaming.com",
-      "http://up365gaming.com",
-      "https://api.up365gaming.com",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: "*",
     credentials: true,
@@ -42,6 +36,13 @@ const io = new Server(server, {
 //       "https://up365gaming.com",
 //       "https://api.up365gaming.com",
 //     ],
+// origin: [
+//   "https://www.up365gaming.com",
+//   "https://up365gaming.com",
+//   "http://www.up365gaming.com",
+//   "http://up365gaming.com",
+//   "https://api.up365gaming.com",
+// ],
 // Same origin allowed for Socket.IO // origin: "https://up365gaming.com" || "http://localhost:3000",
 // methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 // Allowed methods for Socket.IO
